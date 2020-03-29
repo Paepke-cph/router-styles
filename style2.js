6d9e9d688e9a15294f8c9d4024b9a85a9d0d24a4
Lars-m <lam@cphbusiness.dk>
2020-03-29 16:16:52 +0200

    Update style2.js

--- style2.js
+++ style2.js
@@ -1,9 +1,7 @@
 /*
 This is the Horizontal Navigation Bar Example with Active/Current Navigation Link
-
 From here https://www.w3schools.com/css/css_navbar.asp
 */
-
 
 ul.header {
   list-style-type: none;
@@ -11,17 +9,23 @@ ul.header {
   padding: 0;
   overflow: hidden;
   background-color: #333;
- }
- 
- ul.header li { float: left;}
- 
- ul.header li a {
+}
+
+ul.header li {
+  float: left;
+}
+
+ul.header li a {
   display: block;
   color: white;
   text-align: center;
   padding: 14px 16px;
   text-decoration: none;
- }
- ul.header li a:hover:not(.active) { background-color: #111;}
- 
- .active { background-color: #4CAF50;}
+}
+ul.header li a:hover:not(.active) {
+  background-color: #111;
+}
+
+.selected {
+  background-color: #4caf50;
+}
